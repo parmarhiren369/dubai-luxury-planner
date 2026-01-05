bfaf2fbd8a2cbc1f4bd36eb15ef91733fa2e0b06
hirenhp936 <hirenhp936@gmail.com>
2026-01-05 08:16:26 +0000

    fix: Handle missing data in Excel import

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,19 @@
-// src/firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCmw_3M5bCUDZFJr-31NVrQR38HK_bKWfs",
-  authDomain: "wtb-tourism-a6c1d.firebaseapp.com",
-  projectId: "wtb-tourism-a6c1d",
-  storageBucket: "wtb-tourism-a6c1d.firebasestorage.app",
-  messagingSenderId: "1070600261414",
-  appId: "1:1070600261414:web:64bf877abdb7c776613be7",
-  measurementId: "G-TYNWLFLKD1"
+  apiKey: "AIzaSyD_BIz6ZgGdTt-E7TVXvdP3p5UmFZ_wlzI",
+  authDomain: "wtb-tourism-a8017.firebaseapp.com",
+  projectId: "wtb-tourism-a8017",
+  storageBucket: "wtb-tourism-a8017.firebasestorage.app",
+  messagingSenderId: "1069062283622",
+  appId: "1:1069062283622:web:b543482ee2eee4ce6da8a7",
+  measurementId: "G-MD4VVCZCJ8"
 };
 
 // Initialize Firebase
